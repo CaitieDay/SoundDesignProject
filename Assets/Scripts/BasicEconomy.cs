d3b59fe0d9671dad6963703e407eaee264116887
Caitie Day <catherineday608@gmail.com>
2021-06-28 17:16:36 +1000

    First upload of the project

---
+++ Assets/Scripts/BasicEconomy.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicEconomy : MonoBehaviour
+{
+    //VARS
+    int fullHoneyJars;
+    int gold;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/HoneyCollect.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class HoneyCollect : MonoBehaviour
+{
+    //VARS
+    public Camera playerCamera;
+    public float rayRange = 5f;
+    public KeyCode interact;
+    private ResetJars RJ;
+
+    //DATA
+    public Text dataTextJars;
+    public int dataAmountJars = 0; 
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        dataTextJars.text = dataAmountJars.ToString();
+        RJ = new ResetJars();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Ray rayCast = playerCamera.ScreenPointToRay(new Vector2(Screen.width / 2, Screen.height / 2));
+        RaycastHit rayHit;
+
+        if (Physics.Raycast(rayCast, out rayHit, rayRange))
+        {
+            if (rayHit.collider.tag == "Jars")
+            {
+                 if (Input.GetKeyDown(interact))
+                 {
+                    dataAmountJars++;
+                    dataTextJars.text = dataAmountJars.ToString();
+                    RJ.ChangeJar();
+                 }
+            }
+        }
+    }
+}
